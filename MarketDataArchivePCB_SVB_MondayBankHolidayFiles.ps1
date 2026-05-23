b7fcd8d9ae500ed7552386638d8e5d13a4bc082f
Adriano Alves da Silva <58754259+adrianoalves-cwb@users.noreply.github.com>
2026-05-22 22:27:05 -0300

    Update MarketDataArchivePCB_SVB_MondayBankHolidayFiles.ps1

--- MarketDataArchivePCB_SVB_MondayBankHolidayFiles.ps1
+++ MarketDataArchivePCB_SVB_MondayBankHolidayFiles.ps1
@@ -1,11 +1,11 @@
-﻿
+
 
 #ADDING THE LIST OF THE FOLDERS/FILES TYPES TO A VARIABLE
-$folders = @('MARK_PCB','MARK_SVB')
+$folders = @('MARK_PCB', 'MARK_SVB')
 
 #PATH TO THE NETWORK FOLDER AND THE BANK HOLIDAYS FILE
-$networkFolder = '\\vcn.ds.volvo.net\cli-sd\sd0855\043160\01.GDS Database\'
-$bankHolidaysConfigFile = '\\vcn.ds.volvo.net\it-cta\ITPROJ02\002378\DESENV\DBS\AUTOMATOR\MARK_DATA\Automator-MARKET_DATA_Bank_Holidays.txt'
+$networkFolder = '\\fake-corp-fileserver\dept-share\project123\market-data-root\'
+$bankHolidaysConfigFile = '\\fake-corp-config\automation\market-data\config\Bank_Holidays.txt'
 
 #GETTING THE CURRENT MONTH
 $currentMonth = get-date -Format "yyyyMM"
@@ -15,37 +15,31 @@ $dayOfTheWeek = (Get-Date).DayOfWeek
 #READING THE BANK HOLIDAYS CONFIG FILE
 $isTodayBankHoliday = $false
 
-foreach($line in Get-Content $bankHolidaysConfigFile) {
-    if($line -match (get-date -Format 'dd/MM/yyyy')){
+foreach ($line in Get-Content $bankHolidaysConfigFile) {
+    if ($line -match (get-date -Format 'dd/MM/yyyy')) {
         $isTodayBankHoliday = $true
     }
 }
 
 #IF TODAY IS MONDAY OR BANK HOLIDAY, THE PCB AND SVB FILES CAN BE ARCHIVED
-if ($dayOfTheWeek -match "Monday" -or $isTodayBankHoliday)
-{
+if ($dayOfTheWeek -match "Monday" -or $isTodayBankHoliday) {
     #LOOPING THROUGH THE MARKET DATA FOLDER $folders
-    foreach ($folder in $folders)
-    {
+    foreach ($folder in $folders) {
         $sourceFolderPath = $networkFolder + $folder + '\Database from e-mails\'
         $destinationFolderPath = $networkFolder + $folder
 
         #CREATING THE DESTINATION FOLDER PATH BASED ON EACH FOLDER/FILE TYPE
-        switch($folder)
-        {
-            'MARK_PCB' {$destinationFolderPath +=  '\Base_OK\' + $currentMonth }
-            'MARK_SVB' {$destinationFolderPath +=  '\Base_OK\' + $currentMonth }
+        switch ($folder) {
+            'MARK_PCB' { $destinationFolderPath += '\Base_OK\' + $currentMonth }
+            'MARK_SVB' { $destinationFolderPath += '\Base_OK\' + $currentMonth }
         }
     
         #LOOPING THROUGH ALL THE FILES IN THE SOURCE FOLDER AND FILTERING TO ONLY CHECK THE ZIP FILES
-        foreach ($file in (Get-ChildItem $sourcefolderPath -Filter *.zip))
-        {
-            if (!($file.Name -like "*$today*"))
-            {
+        foreach ($file in (Get-ChildItem $sourcefolderPath -Filter *.zip)) {
+            if (!($file.Name -like "*$today*")) {
                 #CONFIRMING THAT THE DESTINATION FOLDER EXISTS. IF IT DOES NOT, THAT IT WILL BE CREATED
                 #The below makes sure the folder named with the current month exists when we reach the 'next' month. For example: 202007 (2020=Year, 07=July)
-                if (!(Test-Path $destinationFolderPath))
-                {
+                if (!(Test-Path $destinationFolderPath)) {
                     #CREATING THE NEW FOLDER
                     New-Item $destinationFolderPath -ItemType Directory
                 }
